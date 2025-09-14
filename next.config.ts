dfbf9476d7cac89c314f71b52bac807fa83d4440
Jussi Nevavuori <jussi@nevavuori.fi>
2025-09-14 12:24:58 +0300

    pin versions, add debug information to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	// Debug information:
+	
+	// This was used to ensure that the issue was Typescript
+	// eslint: { ignoreDuringBuilds: true },
+	
+	// This "fixes" the issue (by skipping type checking entirely)
+	// typescript: { ignoreBuildErrors: true }
 };
 
 export default nextConfig;
